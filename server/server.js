16ef9ee814eb0dd4dcdd795d05c197f04bd7eb59
Harshit Khanna <harshitkhanna3254@gmail.com>
2018-12-28 17:22:20 +0530

    second

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 publicPath = path.join(__dirname + '/../public');
 
 var app = express();
