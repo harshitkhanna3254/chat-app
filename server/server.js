17e3d6f06f54096d37616541389f2e4b96f91c34
Harshit Khanna <harshitkhanna3254@gmail.com>
2018-12-30 20:35:48 +0530

    gobigobigobi

--- server/server.js
+++ server/server.js
@@ -17,12 +17,31 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
     console.log("New user connected.")
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    })
+
+    socket.broadcast.emit('newMessage', {
+            from: 'Admin',
+            text: 'New User joined',
+            createdAt: new Date().getTime()
+        })
+
+
+
     socket.on('disconnect', () => {
         console.log("User disconnected.")
     })
 
     socket.on('createMessage', (message) => {
         console.log("Client wants to send a message", message);
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // })
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
